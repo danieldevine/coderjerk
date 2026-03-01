623f1cba34a8056fdb57bc6a26bd6c0edf339cf7
Dan Devine <dandevine0@gmail.com>
2026-03-01 11:13:02 +0000

    mor tag manager

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -2,10 +2,25 @@ import DefaultTheme from "vitepress/theme";
 import { h } from "vue";
 import PostMeta from "./PostMeta.vue";
 
+const GtmNoscript = {
+  setup() {
+    return () =>
+      h("noscript", null, [
+        h("iframe", {
+          src: "https://www.googletagmanager.com/ns.html?id=GTM-NJZFPW",
+          height: "0",
+          width: "0",
+          style: "display:none;visibility:hidden",
+        }),
+      ]);
+  },
+};
+
 export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
+      "layout-top": () => h(GtmNoscript),
       "doc-before": () => h(PostMeta),
     });
   },
